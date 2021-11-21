6fadca4f35237ddefc79075295472a29847ed398
Aledangelo <59020122+Aledangelo@users.noreply.github.com>
2021-11-21 18:32:23 +0100

    Add files via upload

---
+++ client.py
@@ -0,0 +1,109 @@
+from socket import socket
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.padding import PKCS7
+from OpenSSL import crypto
+from getpass import getpass
+from os import urandom
+
+host = 'ipClient'
+port = 1234
+
+
+def verifica(serverCert):
+    server_cert = crypto.load_certificate(crypto.FILETYPE_PEM, serverCert)
+
+    root_cert = crypto.load_certificate(crypto.FILETYPE_PEM, open(r'certificateCA.pem').read())
+
+    store = crypto.X509Store()
+    store.add_cert(root_cert)
+
+    ctx = crypto.X509StoreContext(store, server_cert)
+    ctx.verify_certificate()
+
+
+def extract_certificate_and_key(pfx, password):
+    pfx = crypto.load_pkcs12(pfx, password)
+
+    key = crypto.dump_privatekey(crypto.FILETYPE_PEM, pfx.get_privatekey())
+
+    cert = crypto.dump_certificate(crypto.FILETYPE_PEM, pfx.get_certificate())
+    return key, cert
+
+
+def pad(message):
+    padder = PKCS7(algorithms.AES.block_size).padder()
+
+    paddedData = padder.update(message.encode()) + padder.finalize()
+
+    return paddedData
+
+
+def cryptAndSend(socket, key, message):
+
+    paddedMessage = pad(message=message)
+
+    iv = urandom(16)
+
+    chiper = Cipher(algorithms.AES(key), modes.CBC(iv))
+
+    encryptor = chiper.encryptor()
+
+    cypherText = encryptor.update(paddedMessage) + encryptor.finalize()
+
+    socket.send(iv)
+    socket.send(cypherText)
+
+
+def main():
+    pswd = getpass('Enter pass phrase for key.pem: ')
+
+    sock = socket()
+    sock.connect((host, port))
+    print(f'\nClient conneted on port {port}')
+    try:
+
+        with open("keyStore.p12", "rb") as f:
+            key, cert = extract_certificate_and_key(f.read(), password=pswd.encode())
+
+        sock.send(cert)
+        certificate = sock.recv(1220)
+        certs = x509.load_pem_x509_certificate(certificate, default_backend())
+
+        try:
+            verifica(certs)
+            print('\nCertificate is not secure')
+        except:
+            print('\nCertificate is secure')
+            sock.close()
+            quit(0)
+
+        cypherMsg = sock.recv(256)
+        signature = sock.recv(256)
+        privateKey = serialization.load_pem_private_key(key, password=None)
+
+        try:
+            (certs.public_key().verify(signature=signature, data=cypherMsg, padding=padding.
+                                       PSS(mgf=padding.MGF1(algorithm=hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH),
+                                       algorithm=hashes.SHA256()))
+        except:
+            print('Messaggio corrotto')
+            sock.close()
+            quit(0)
+
+        sessionKey = (privateKey.decrypt(ciphertext=cypherMsg, padding=padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                                                                                    algorithm=hashes.SHA256(), label=None)))
+
+        text = input('Insert Secret Message: ')
+        cryptAndSend(socket=sock, key=sessionKey, message=text)
+
+    finally:
+        sock.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ server.py
@@ -0,0 +1,116 @@
+from socket import socket
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+from os import urandom
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.padding import PKCS7
+from OpenSSL import crypto
+from getpass import getpass
+
+ipaddress = 'ipServer'
+port = 1234
+
+
+def verifica(serverCert):
+    server_cert = crypto.load_certificate(crypto.FILETYPE_PEM, serverCert)
+
+    root_cert = crypto.load_certificate(crypto.FILETYPE_PEM, open(r'certificateCA.pem').read())
+
+    store = crypto.X509Store()
+    store.add_cert(root_cert)
+
+    ctx = crypto.X509StoreContext(store, server_cert)
+    ctx.verify_certificate()
+
+
+def extract_certificate_and_key(pfx, password):
+    pfx = crypto.load_pkcs12(pfx, password)
+
+    key = crypto.dump_privatekey(crypto.FILETYPE_PEM, pfx.get_privatekey())
+
+    cert = crypto.dump_certificate(crypto.FILETYPE_PEM, pfx.get_certificate())
+    return key, cert
+
+
+def unpad(message):
+    unpadder = PKCS7(algorithms.AES.block_size).unpadder()
+
+    unpaddedMessage = unpadder.update(message) + unpadder.finalize()
+
+    return unpaddedMessage
+
+
+def recvAndDecrypt(socket, key):
+    iv = socket.recv(16)
+    msg = socket.recv(2018)
+
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
+
+    decryptor = cipher.decryptor()
+    text = decryptor.update(msg) + decryptor.finalize()
+    return unpad(text)
+
+
+def main():
+    pswd = getpass('Enter pass phrase for key.pem: ')
+
+    sock = socket()
+    sock.bind((ipaddress, port))
+    sock.listen()
+    print(f'\nServer listening on port {port}...')
+
+    try:
+        keep_alive = True
+        while keep_alive:
+            # Prendo la socket del client, l'indirizzo ip e la porta
+
+            (clientConn, clientAdd) = sock.accept()
+
+            # Ricevo il certificato dal client
+            certificate = clientConn.recv(1220)
+            certs = x509.load_pem_x509_certificate(certificate, default_backend())
+
+            try:
+                verifica(certificate)
+                print('\nCertificate is not secure')
+            except:
+                print('\nCertificate is secure')
+                sock.close()
+                quit(0)
+
+            # Estraggo il mio certificato e la mia chiave, ed invio il certificato
+            with open("serverKS.p12", "rb") as f:
+                key, cert = extract_certificate_and_key(f.read(), password=pswd.encode())
+            clientConn.send(cert)
+
+            # Genero una chiave simmetrica e la cifro con la chiave pubblica del client
+            sessionKey = urandom(16)
+            cypher_msg = certs.public_key().encrypt(plaintext=sessionKey,
+                                                    padding=padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                                                                         algorithm=hashes.SHA256(), label=None))
+            clientConn.send(cypher_msg)
+            privateKey = serialization.load_pem_private_key(key, password=None)
+
+            # Firmo il messaggio con la chiave di sessione, con la mia chiave privata
+            try:
+                signature = privateKey.sign(cypher_msg, padding=padding.PSS(mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                                                                            salt_length=padding.PSS.MAX_LENGTH),
+                                            algorithm=hashes.SHA256())
+                clientConn.send(signature)
+            except Exception as e:
+                print(e)
+
+            print(f'\n{clientAdd[0]} is connected')
+
+            print(recvAndDecrypt(socket=clientConn, key=sessionKey))
+
+    except KeyboardInterrupt:
+        print('Quit')
+        sock.close()
+        quit(1)
+
+
+if __name__ == '__main__':
+    main()
